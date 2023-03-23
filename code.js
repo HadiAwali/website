23761ef762627b58c1e8e0edb23bb1185c7c680e
Anonymous <98743369+HadiAwali@users.noreply.github.com>
2023-03-23 05:36:16 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 <script type="module">
   import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
-  const swiper = new Swiper(...)
 </script>
 
 window.mySwipe = new Swipe (document.getElementById ('slider'), {
