e860b5a4b1f8c4164f0edf3326aefd4fc6c7b652
Anonymous <98743369+HadiAwali@users.noreply.github.com>
2023-03-23 05:00:48 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,10 @@
+window.mySwipe = new Swipe (document.getElementById ('slider'), {
+  startSlide: 2,
+  speed: 400,
+  auto: 3000,
+  continuous: true,
+  disableScroll: false,
+  stopPropagation: false,
+  callback: function (index, elem) {},
+  transitionEnd: function (index, elem) {}
+});
